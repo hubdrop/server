f03ab22b919cfb3cbb5a28cdb3c2869b76452cbe
Jon Pugh <jon@thinkdrop.net>
2013-03-13 11:23:07 -0400

    getting rid of sql reset... that was trouble!

--- sql_reset/reset.sh
+++
@@ -1,5 +0,0 @@
-/etc/init.d/mysql stop
-/usr/sbin/mysqld --skip-grant-tables --skip-networking &
-mysql -u root < "FLUSH PRIVILEGES; SET PASSWORD FOR root@'localhost' = PASSWORD('$1'); UPDATE mysql.user SET Password=PASSWORD('$1') WHERE User='root'; FLUSH PRIVILEGES;"
-/etc/init.d/mysql stop
-/etc/init.d/mysql start
