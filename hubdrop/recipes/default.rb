db0805d874e8c94b7f007f3ced10cfc66fdbb96b
Jon Pugh <jon@thinkdrop.net>
2013-11-04 19:59:20 -0500

    Adding jenkins server.

---
+++ hubdrop/recipes/default.rb
@@ -0,0 +1,11 @@
+ 
+ 
+# Install Jenkins server
+include_recipe "jenkins::server"
+
+# Deploy hubdrop Symfony app
+# @TODO: Write hubdrop Symfony app
+# @TODO: Learn and use deploy LWRP
+
+# Deploy hubdrop server scripts
+# @TODO: Learn and use deploy LWRP
