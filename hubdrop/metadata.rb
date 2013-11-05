c0d07f7fe396519ea8b2fd4e1633573f244ab2d2
Jon Pugh <jon@thinkdrop.net>
2013-11-05 10:25:50 -0500

    adding metadata.rb

---
+++ hubdrop/metadata.rb
@@ -0,0 +1,12 @@
+name             'hubdrop'
+maintainer       'ThinkDrop Consulting, LLC'
+maintainer_email 'cookbookbooks@thinkdrop.net'
+license          'GPLv2'
+description      'Creates hubdrop.io server.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.0.0'
+
+recipe 'default', 'Installs a fully working hubdrop.io server.'
+
+depends 'jenkins::server'
+
