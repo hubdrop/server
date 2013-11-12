2a1e7bd5128d6f46bfed80dc523ce36def1fb769
Jon Pugh <jon@thinkdrop.net>
2013-11-12 15:48:04 -0500

    fixing package-cookbooks script

--- package-cookbooks.sh
+++ package-cookbooks.sh
@@ -1,4 +1,5 @@
-tar zcvf chef-solo.tar.gz ../cookbooks
+rm chef-solor.tar.gz
+tar zcvf chef-solo.tar.gz ../cookbooks --exclude .git --exclude chef-solo.tar.gz
 git add chef-solo.tar.gz
 git commit -m 'HubDrop: Cookbook Package Updated.'
 git push
