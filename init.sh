3b132dd1a48f196d2ee58b7d5b9a5bebf9c531e2
Jon Pugh <jon@thinkdrop.net>
2014-01-08 12:16:59 -0500

    Adding /etc/hosts setting.

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 git clone git@github.com:hubdrop/cookbooks.git
 git clone git@github.com:hubdrop/app.git
+
+# @TODO: Let's make init.sh a wizard, and ask these things
 cp attributes.json.example attributes.json
+echo '1.1.1.1  hubdrop.local' | sudo tee -a /etc/hosts
