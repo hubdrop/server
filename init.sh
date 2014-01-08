ac93cfca2c9ae50c07e55aea10fa5a7e950ae06a
Jon Pugh <jon@thinkdrop.net>
2014-01-08 17:01:28 -0500

    updating the IP address

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ git clone git@github.com:hubdrop/app.git
 
 # @TODO: Let's make init.sh a wizard, and ask these things
 cp attributes.json.example attributes.json
-echo '1.1.1.1  hubdrop.local' | sudo tee -a /etc/hosts
+echo '10.10.10.10  hubdrop.local' | sudo tee -a /etc/hosts
