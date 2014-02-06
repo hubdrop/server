66b5fd26ce4d0b79f60c9c3c253c6872f0ff06b8
Jon Pugh <jon@thinkdrop.net>
2014-02-05 19:38:37 -0500

    Changing our environment variables.

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ cp attributes.json.example attributes.json
 echo '10.10.10.10  hubdrop.local' | sudo tee -a /etc/hosts
 
 export SYMFONY__APP__URL='http://hubdrop.local'
-export SYMFONY__GITHUB__USERNAME='hubdrop-user'
-export SYMFONY__GITHUB__ORGANIZATION='drupalprojects'
-export SYMFONY__GITHUB__AUTHORIZATION='4a8c4d54758db858bc912169f3e51fafd1fff5f9'
-export SYMFONY__DRUPAL__USERNAME='jon pugh'
+export SYMFONY__APP__GITHUB__USERNAME='hubdrop-user'
+export SYMFONY__APP__GITHUB__ORGANIZATION='drupalprojects'
+export SYMFONY__APP__GITHUB__AUTHORIZATION='4a8c4d54758db858bc912169f3e51fafd1fff5f9'
+export SYMFONY__APP__DRUPAL__USERNAME='jon pugh'
